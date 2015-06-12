f3e292ed9c4b966c994341d806f034582f62bf9c
Scott Lovegrove <scottisafool@live.co.uk>
2015-06-12 19:26:27 +0100

    Initial PrinterService work

---
+++ src/Cimbalino.Toolkit (Win10)/Services/PrinterService.cs
@@ -0,0 +1,114 @@
+﻿#if WINDOWS_UAP || WINDOWS_APP
+using System;
+using System.Threading.Tasks;
+using Windows.Graphics.Printing;
+using Cimbalino.Toolkit.Helpers;
+
+#else
+using System;
+using System.Threading.Tasks;
+using Cimbalino.Toolkit.Helpers;
+#endif
+
+namespace Cimbalino.Toolkit.Services
+{
+    public class PrinterService : IPrinterService
+    {
+#if WINDOWS_UAP || WINDOWS_APP
+        public PrinterService()
+        {
+            PrintManager.GetForCurrentView().PrintTaskRequested += OnPrintTaskRequested;
+        }
+
+        private void OnPrintTaskRequested(PrintManager sender, PrintTaskRequestedEventArgs args)
+        {
+            var eventHandler = PrintTaskRequested;
+            eventHandler?.Invoke(this, new PrinterRequestEventArgs(new PrinterServiceTask(args)));
+        }
+
+        public event EventHandler<PrinterRequestEventArgs> PrintTaskRequested;
+        public event EventHandler<PrinterTaskCompletedEventArgs> PrintTaskCompleted;
+#else
+        public event EventHandler<PrinterRequestEventArgs> PrintTaskRequested
+        {
+            add
+            {
+                ExceptionHelper.ThrowNotSupported();
+            }
+            remove
+            {
+                
+            }
+        }
+
+        public event EventHandler<PrinterTaskCompletedEventArgs> PrintTaskCompleted
+        {
+            add
+            {
+                ExceptionHelper.ThrowNotSupported();
+            }
+            remove
+            {
+
+            }
+        }
+#endif
+
+        public Task<bool> ShowAsync()
+        {
+#if WINDOWS_UAP || WINDOWS_APP
+            return PrintManager.ShowPrintUIAsync().AsTask();
+#else
+            return ExceptionHelper.ThrowNotSupported<Task<bool>>();
+#endif
+        }
+    }
+
+    public class PrinterServiceTask : IPrinterServiceTask
+    {
+#if WINDOWS_UAP || WINDOWS_APP
+        private readonly PrintTaskRequestedEventArgs _args;
+
+        private PrintTaskRequestedDeferral _deferral;
+
+        public PrinterServiceTask(PrintTaskRequestedEventArgs args)
+        {
+            _args = args;
+        }
+#endif
+
+        public void Complete()
+        {
+#if WINDOWS_UAP || WINDOWS_APP
+            if (_deferral == null)
+            {
+                ExceptionHelper.ThrowNotSupported();
+                return;
+            }
+
+            _deferral.Complete();
+#else
+            ExceptionHelper.ThrowNotSupported();
+#endif
+        }
+
+        public void GetDeferral()
+        {
+#if WINDOWS_UAP || WINDOWS_APP
+            var request = _args?.Request;
+            _deferral = request?.GetDeferral();
+#else
+            ExceptionHelper.ThrowNotSupported();
+#endif
+        }
+
+        public void CreatePrintTask(string title)
+        {
+#if WINDOWS_UAP || WINDOWS_APP
+
+#else
+            ExceptionHelper.ThrowNotSupported();
+#endif
+        }
+    }
+}
---
+++ src/Cimbalino.Toolkit.Core (Portable)/Services/IPrinterService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Cimbalino.Toolkit.Services
+{
+    public interface IPrinterService
+    {
+        event EventHandler<PrinterRequestEventArgs> PrintTaskRequested;
+        event EventHandler<PrinterTaskCompletedEventArgs> PrintTaskCompleted;
+        Task<bool> ShowAsync();
+    }
+
+    public class PrinterRequestEventArgs : EventArgs
+    {
+        public PrinterRequestEventArgs(IPrinterServiceTask request)
+        {
+            Request = request;
+        }
+
+        public IPrinterServiceTask Request { get; set; }
+    }
+
+    public class PrinterTaskCompletedEventArgs : EventArgs
+    {
+        public PrinterTaskCompletedEventArgs(PrinterTaskCompletion completion)
+        {
+            Completion = completion;
+        }
+
+        public PrinterTaskCompletion Completion { get; set; }
+    }
+
+    public interface IDeferral
+    {
+        void Complete();
+        void GetDeferral();
+    }
+
+    public interface IPrinterServiceTask : IDeferral
+    {
+        void CreatePrintTask(string title);
+    }
+
+    public enum PrinterTaskCompletion
+    {
+        //
+        // Summary:
+        //     An abandoned print task.
+        Abandoned = 0,
+        //
+        // Summary:
+        //     A canceled print task.
+        Canceled = 1,
+        //
+        // Summary:
+        //     A failed print task.
+        Failed = 2,
+        //
+        // Summary:
+        //     A submitted print task.
+        Submitted = 3
+    }
+}
