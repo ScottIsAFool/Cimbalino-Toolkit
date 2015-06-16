ffa283c5157ac81095d51259d97d2661e0c30b57
Scott Lovegrove <scottisafool@live.co.uk>
2015-06-16 18:14:37 +0100

    Initial ShareService work

---
+++ src/Cimbalino.Toolkit.Core (Portable)/Services/IShareContractService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Cimbalino.Toolkit.Services
+{
+    public interface IShareContractService
+    {
+        void ShowUIAsync();
+
+        event EventHandler<ShareDataRequestedEventArgs> DataRequested;
+    }
+
+    public class ShareDataRequestedEventArgs : EventArgs
+    {
+        public ShareDataRequestedEventArgs(IShareDataRequest request)
+        {
+            Request = request;
+        }
+
+        public IShareDataRequest Request { get; private set; }
+    }
+
+    public interface IShareDataRequest : IDeferral
+    {
+        void FailWithDisplayText(string text);
+        void SetApplicationLink(Uri value);
+        void SetData(string formatId, object value);
+        void SetHtmlFormat(string value);
+        void SetRtf(string value);
+        void SetText(string value);
+        void SetWebLink(Uri value);
+    }
+}
