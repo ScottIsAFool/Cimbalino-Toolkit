9a1837e0504612ec69ecd7199a1851bd3b832a9c
Scott Lovegrove <scottisafool@live.co.uk>
2015-06-16 18:14:37 +0100

    Initial ShareService work

---
+++ src/Cimbalino.Toolkit (WPA81)/Services/ShareContractService.cs
@@ -0,0 +1,159 @@
+﻿#if WINDOWS_PHONE
+using System;
+using Cimbalino.Toolkit.Helpers;
+#else
+using System;
+using Windows.ApplicationModel.DataTransfer;
+
+#endif
+
+namespace Cimbalino.Toolkit.Services
+{
+    public class ShareContractService : IShareContractService
+    {
+#if !WINDOWS_PHONE
+        public event EventHandler<ShareDataRequestedEventArgs> DataRequested;
+
+        public void ShowUIAsync()
+        {
+            var manager = DataTransferManager.GetForCurrentView();
+            manager.DataRequested += ManagerOnDataRequested;
+
+            DataTransferManager.ShowShareUI();
+        }
+
+        private void ManagerOnDataRequested(DataTransferManager sender, DataRequestedEventArgs args)
+        {
+            var request = args?.Request;
+            sender.DataRequested -= ManagerOnDataRequested;
+
+            var eventHandler = DataRequested;
+            eventHandler?.Invoke(this, new ShareDataRequestedEventArgs(new ShareDataRequest(request)));
+        }
+#else
+        public event EventHandler<ShareDataRequestedEventArgs> DataRequested
+        {
+            add
+            {
+                ExceptionHelper.ThrowNotSupported();
+            }
+            remove
+            {
+
+            }
+        }
+
+        public void ShowUIAsync()
+        {
+            ExceptionHelper.ThrowNotSupported();
+        }
+#endif
+    }
+
+    public class ShareDataRequest : IShareDataRequest
+    {
+#if !WINDOWS_PHONE
+        private readonly DataRequest _request;
+        private DataRequestDeferral _deferral;
+        internal ShareDataRequest(DataRequest request)
+        {
+            _request = request;
+        }
+
+        public void Complete()
+        {
+            if (_deferral == null)
+            {
+                throw new InvalidOperationException("You must call GetDeferral() before calling this method");
+            }
+
+            _deferral.Complete();
+        }
+
+        public void GetDeferral()
+        {
+            _deferral = _request?.GetDeferral();
+        }
+
+        public void FailWithDisplayText(string text)
+        {
+            _request?.FailWithDisplayText(text);
+        }
+
+        public void SetApplicationLink(Uri value)
+        {
+            _request?.Data?.SetApplicationLink(value);
+        }
+
+        public void SetData(string formatId, object value)
+        {
+            _request?.Data?.SetData(formatId, value);
+        }
+
+        public void SetHtmlFormat(string value)
+        {
+            _request?.Data?.SetHtmlFormat(value);
+        }
+
+        public void SetRtf(string value)
+        {
+            _request?.Data?.SetRtf(value);
+        }
+
+        public void SetText(string value)
+        {
+            _request?.Data?.SetText(value);
+        }
+
+        public void SetWebLink(Uri value)
+        {
+            _request?.Data?.SetWebLink(value);
+        }
+#else
+        public void Complete()
+        {
+            ExceptionHelper.ThrowNotSupported();
+        }
+
+        public void GetDeferral()
+        {
+            ExceptionHelper.ThrowNotSupported();
+        }
+
+        public void FailWithDisplayText(string text)
+        {
+            ExceptionHelper.ThrowNotSupported();
+        }
+
+        public void SetApplicationLink(Uri value)
+        {
+            ExceptionHelper.ThrowNotSupported();
+        }
+
+        public void SetData(string formatId, object value)
+        {
+            ExceptionHelper.ThrowNotSupported();
+        }
+
+        public void SetHtmlFormat(string value)
+        {
+            ExceptionHelper.ThrowNotSupported();
+        }
+
+        public void SetRtf(string value)
+        {
+            ExceptionHelper.ThrowNotSupported();
+        }
+
+        public void SetText(string value)
+        {
+            ExceptionHelper.ThrowNotSupported();
+        }
+
+        public void SetWebLink(Uri value)
+        {
+            ExceptionHelper.ThrowNotSupported();
+        }
+#endif
+    }
+}
